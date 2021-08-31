5a1c837a1b7a5c65bebefbf382a0901eb7b518ef
Fatih Koç <fatihkoc@hotmail.it>
2021-08-31 21:57:02 +0300

    Static resources added to project

--- pet-clinic-web/src/main/java/com/fatihkocc9/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/fatihkocc9/sfgpetclinic/bootstrap/DataLoader.java
@@ -23,14 +23,12 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Fatih");
         owner1.setLastName("Koc");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Ozge");
         owner2.setLastName("Tatar");
 
@@ -39,14 +37,12 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded 2 owners");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Kevser");
         vet1.setLastName("Koc");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Nesenur");
         vet2.setLastName("Koc");
 
--- pet-clinic-web/src/main/java/com/fatihkocc9/sfgpetclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/com/fatihkocc9/sfgpetclinic/controller/OwnerController.java
@@ -19,7 +19,6 @@ public class OwnerController {
     @RequestMapping({"", "/", "/index", "/index.html"})
     public String listOwners(Model model) {
         model.addAttribute("owners",ownerService.findAll());
-
         return "owners/index";
     }
 
